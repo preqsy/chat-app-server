34fe3c3474312aaca4ade8a0a408896f9e47d633
Precious Ohanyere <obbyprecious24@gmail.com>
2025-04-19 01:06:07 +0100

    fix: Update CORS policy to allow requests from production frontend

--- main.go
+++ main.go
@@ -68,7 +68,7 @@ func main() {
 
 	corsHandler := func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+			w.Header().Set("Access-Control-Allow-Origin", "https://chat-app-frontend-production-bb6d.up.railway.app")
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
